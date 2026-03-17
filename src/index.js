a28da3cc45dafe057abbcb4723bfb75392c49507
Karthikeyan Iyer <karthikeyan.iyer@symphonyai.com>
2026-03-17 18:01:06 +0530

    fixed tmux rendering

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const http = require('http');
+const path = require('path');
 const WebSocket = require('ws');
 const { handleAgentConnection, handleClientConnection } = require('./relay');
 const devicesRouter = require('./devices');
 
 const app = express();
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  if (req.method === 'OPTIONS') return res.sendStatus(204);
+  next();
+});
 app.use(express.json({ limit: '10kb' }));
+app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use('/', devicesRouter);
 
 const server = http.createServer(app);
